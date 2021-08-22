892d5fd6ff2241020d2fef6a2cf05ab3d0b73eee
ProstoIgor <igor.lyzar@yandex.ru>
2021-08-22 18:19:28 +0300

    выполнил чек лист

--- README.md
+++ README.md
@@ -1,28 +1,11 @@
 # Проект: Место
+------------------------
+[Проект на GitHub](https://prostoigor.github.io/Russian_travel/)
+-----------------------
+В этом проекте использовались технологии:
+ - Flexbox;
+ - Grid;
+ - Медиазапросы;
+ - Адаптивной верстки;
+ - Весь проект выполнен по методолгии БЭМ.
 
-### Обзор
-
-* Figma
-* Картинки
-
-**Figma**
-
-* [Ссылка на макет в Figma](https://www.figma.com/file/2cn9N9jSkmxD84oJik7xL7/JavaScript.-Sprint-4?node-id=0%3A1)
-
-**Картинки**
-
-Доставать картинки предстоит из Фигмы. Это расхожая практика, поэтому полезно потренироваться.
-Не забудьте [оптимизировать картинки](https://tinypng.com/), чтобы ваш сайт загружался быстрее.
-
-Удачи!
-
-<div class="popup popup_is-opened">
-    <button type="button" class="popup__close">&times</button>
-    <div class="popup__container">
-      <h2 class="popup__title">Редактировать профиль</h2>
-      <form class="popup__form" name="new-user">
-        <input type="text" id="name" name='name' placeholder="Имя" class="popup__input popup__input_name" required>
-        <input type="text" id="biography" name='biography' placeholder="О себе" class="popup__input popup__input_biography" required>
-        <button type="submit" class="popup__save">Сохранить</button>
-      </form>
-    </div>
--- scripts/script.js
+++ scripts/script.js
@@ -1,32 +1,30 @@
-function() {
-  let formElement = document.querySelector('.popup__form');
-  let nameInput = document.querySelectorAll('.popup__input')[0];
-  let biographyInput = document.querySelectorAll('.popup__input')[1];
-  let popup = document.querySelector('.popup');
-  let editBtn = document.querySelector('.profile__button-edit');
-  let profileName = document.querySelector('.profile__name');
-  let profileBio = document.querySelector('.profile__bio');
+let editButton = document.querySelector('.profile__button-edit');
+let profileName = document.querySelector('.profile__name');
+let profileBio = document.querySelector('.profile__bio');
+let popup = document.querySelector('.popup');
+let closeButton = document.querySelector('.popup__close');
+let formElement = document.querySelector('.popup__form');
+let nameInput = document.querySelectorAll('.popup__input')[0];
+let bioInput = document.querySelectorAll('.popup__input')[1];
 
-function openPopup() {
+function editProfile() {
   popup.classList.add('popup_opened');
-  nameInput.value = profileName.textContent;
-  biographyInput.value = profileBio.textContent;
 }
 
-function closePopup() {
-  popup.classList.remove('popup_close');
+function closeProfile() {
+  popup.classList.remove('popup_opened');
 }
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
-  profileBio.textContent = biographyInput.value;
-  closePopup();
+  profileBio.textContent = bioInput.value;
+
+  nameInput.value = profileName.textContent;
+  bioInput.value.value = profileBio.textContent;
+  closeProfile();
 }
 
+editButton.addEventListener('click', editProfile);
+closeButton.addEventListener('click', closeProfile);
 formElement.addEventListener('submit', formSubmitHandler);
-editBtn.addEventListener('click', openPopup);
-
-let closeBtn = document.querySelector('.popup__close');
-closeBtn.addEventListener('click', closePopup);
-}
